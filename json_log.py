13189a41e30295a3bad906d45809f9bab111da8e
Tobias Widen <tobbetrollkarl@gmail.com>
2015-12-26 08:03:13 +0100

    Making python lib

---
+++ json_log.py
@@ -0,0 +1,42 @@
+import json
+from datetime import datetime
+
+
+def getLogger(name, **kwargs):
+    return JsonLogger(name)
+
+
+class JsonLogger(object):
+
+    class Log(object):
+
+        def __init__(self, logger, name, level):
+            self.logger = logger
+            self.name = name
+            self.level = level
+
+        def __call__(self, **kwargs):
+            d = kwargs.copy()
+            d['_utc_timestamp'] = datetime.utcnow().isoformat()
+            d['_component'] = self.name
+            d['_level'] = self.level
+            self.logger(json.dumps(d))
+
+    def __init__(self, name):
+        import logging
+        self.name = name
+        logging.basicConfig(format='')
+        self.logger = logging.getLogger(name)
+
+        levels = ('info', 'warning', 'error', 'critical', 'exception')
+        for level in levels:
+            setattr(self, level, self.Log(getattr(self.logger, level), self.name, level))
+
+    def setLevel(self, level):
+        self.logger.setLevel(level)
+
+
+# l = getLogger('foo')
+# import logging
+# l.setLevel(logging.DEBUG)
+# l.info(a='b', c='123')
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='twiden-logging',
+    version='1.0.0',
+    description='Log stuff on JSON format',
+    license='MIT',
+    author='Tobias Widén',
+    author_email='tobbetrollkarl@gmail.com',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=['twiden', 'twiden.logging'],
+    install_requires=[]
+    dependency_links=[]
+)
